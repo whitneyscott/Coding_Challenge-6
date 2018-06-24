67d297b8a3c64699bbfed4b0fda979178e08ac35
whitneyscott <swhitney@sfasu.edu>
2018-06-23 23:37:02 -0500

    got rid of annoying alert

--- js/codingCompetition6.js
+++ js/codingCompetition6.js
@@ -39,7 +39,6 @@ document.getElementById("result1").innerHTML=displaytxt;
 
 let arr2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
 function getAddends(input){
-  alert("The input value is: "+input);
   let myVal = parseInt(input);
   slicer = Math.floor(myVal/2)+1
   let slicedArr = arr2.slice(0,slicer);//need + 1 bc arrays start with index = 0
